bc8fec60ee7a90f39cfafe8243f3cfed3a8169f9
Thomas  Henaff <thomas.henaff@epsi.fr>
2021-10-13 11:00:52 +0200

    modif flip

--- Commandes/flip.js
+++ Commandes/flip.js
@@ -1,3 +1,4 @@
+const { randomInt } = require("crypto");
 const Discord = require("discord.js");
 const { PREFIX } = require("../config.js");
 const economy = new db.table("Economy")
@@ -9,7 +10,7 @@ module.exports.run = (client, message, args) => {
 
     let user = message.mentions.members.first() || message.author;
 
-    if (args[1] === 1) {
+    if (args[1] === randomInt(0, 1)) {
         economy.add(`money_${message.guild.id}_${user.id}`, 50)
         return message.channel.send(`Vous avez gagné ${args[1]}`)
     }
